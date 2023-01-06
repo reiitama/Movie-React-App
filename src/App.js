6dcc8d4a198ed8afc5e11b4ecd24cc68d87c3cfc
reiitama <rhwijaya15@gmail.com>
2023-01-06 23:59:11 +0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { getMovieList, searchMovie } from "./api"
+import { useEffect, useState } from "react"
 
-function App() {
+const App = () => {
+  const [RateMovies, setRateMovies] = useState([])
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setRateMovies(result)
+    })
+  }, [])
+
+const RateMovieList = () => {
+    return RateMovies.map((movie, i) => {
+      return (
+        <div className="mov-wrap" key={i}>
+          <div className="mov-title">{movie.title}</div>
+          <img
+            className="mov-image"
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+          />
+          <div className="mov-rate">Rating : {movie.vote_average}</div>
+          <div className="mov-date">Release Date : {movie.release_date}</div>
+        </div>
+      )
+    })
+  }
+
+  const search = async (q) => {
+    const query = await searchMovie(q)
+    // setRateMovies(query.results)
+    console.log({query: query})
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movie List by Reitama</h1>
+        <input 
+        placeholder='Search' 
+        className='mov-search' 
+        onChange={( target ) => search(target.value)}
+        />
+        <div className="mov-container">
+          <RateMovieList />
+        </div>
       </header>
     </div>
   );
